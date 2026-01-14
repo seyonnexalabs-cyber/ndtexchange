00b0c199a13f8cdecad86b5b5d0e1d1d62045e74
SEYON NEXA LABS <seyonnexalabs@gmail.com>
2026-01-14 14:03:59 +0000

    font "Noto sans"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
+        body: ['Noto Sans', 'sans-serif'],
         headline: ['Playfair Display', 'serif'],
         code: ['monospace'],
       },

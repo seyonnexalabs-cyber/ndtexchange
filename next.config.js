0ae1f9b0b06d7520009c039ff4100dee986e0516
NEXUS TECHSMITH <seyonnexalabs@gmail.com>
2026-02-05 18:46:01 +0000

    PWA ready ? how can i give this as mobile app from login without public

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+
 const withPWAInit = require('@ducanh2912/next-pwa').default;
 
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */

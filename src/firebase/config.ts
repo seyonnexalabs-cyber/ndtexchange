d7d5941365daf7b98f4914560fc5565b1ba60265
SEYON NEXA LABS <seyonnexalabs@gmail.com>
2026-02-07 15:12:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1715864548-4fd5d",
+  "appId": "1:489243011755:web:60b8fcb390f1d23062a5ee",
+  "apiKey": "AIzaSyDYZlGVP9Zux3q3Zp_SbfA5bwJE_9KEBCI",
+  "authDomain": "studio-1715864548-4fd5d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "489243011755"
+};

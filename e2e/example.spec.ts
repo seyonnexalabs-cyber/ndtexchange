7ce227b76b20675eb3f3b2abdaa913b1872c1233
SEYON NEXA LABS <seyonnexalabs@gmail.com>
2026-02-07 14:59:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ e2e/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/The Digital Marketplace for Asset Integrity/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Sign Up Free' }).click();
+
+  // Expects the URL to contain signup.
+  await expect(page).toHaveURL(/.*signup/);
+});

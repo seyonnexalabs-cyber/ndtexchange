28e2b669b4bd7c8ba978e05e54186593f229f504
SEYON NEXA LABS <seyonnexalabs@gmail.com>
2026-03-06 09:06:00 +0000

    webpage_content_reporter.js:1 Uncaught SyntaxError: Unexpected token 'ex

--- src/lib/tasks-data.ts
+++ src/lib/tasks-data.ts
@@ -1,2 +1,1 @@
-// This file is deprecated. The constants have been moved to src/lib/seed-data.ts for better organization.
-export {};
+// This file is deprecated. The constants have been moved to src/lib/seed-data.ts and this file can be removed.

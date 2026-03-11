a0833fcbd3aba27e608d92c5c3b80bf26236c85f
SEYON NEXA LABS <seyonnexalabs@gmail.com>
2026-03-11 08:50:46 +0000

    sign up/login page displays the hero image perfectly. but not in other p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 
 'use client';
 
-import type { Metadata } from 'next';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Building, HardHat, Factory, CheckCircle } from 'lucide-react';
